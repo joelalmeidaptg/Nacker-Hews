50b694c4097a1201dcb23ef8620147fe436a9af0
Joel Almeida <joelalmeidaptg@gmail.com>
2017-05-09 16:17:36 +0100

    Enable sending emails in development mode for password recovery.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,53 @@ class User < ApplicationRecord
   validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
   
   def self.find_for_database_authentication(warden_conditions)
-      conditions = warden_conditions.dup
-      if login = conditions.delete(:login)
-        where(conditions.to_h).where(["username = :value OR email = :value", { :value => login.downcase }]).first
-      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-        where(conditions.to_h).first
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["username = :value OR email = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+  
+  protected
+
+  # Attempt to find a user by it's email. If a record is found, send new
+  # password instructions to it. If not user is found, returns a new user
+  # with an email not found error.
+  def self.send_reset_password_instructions attributes = {}
+    recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    recoverable.send_reset_password_instructions if recoverable.persisted?
+    recoverable
+  end
+
+  def self.find_recoverable_or_initialize_with_errors required_attributes, attributes, error = :invalid
+    (case_insensitive_keys || []).each {|k| attributes[k].try(:downcase!)}
+
+    attributes = attributes.slice(*required_attributes)
+    attributes.delete_if {|_key, value| value.blank?}
+
+    if attributes.size == required_attributes.size
+      if attributes.key?(:login)
+        login = attributes.delete(:login)
+        record = find_record(login)
+      else
+        record = where(attributes).first
       end
     end
+
+    unless record
+      record = new
+
+      required_attributes.each do |key|
+        value = attributes[key]
+        record.send("#{key}=", value)
+        record.errors.add(key, value.present? ? error : :blank)
+      end
+    end
+    record
+  end
+
+  def self.find_record login
+    where(["username = :value OR email = :value", {value: login}]).first
+  end
 end
